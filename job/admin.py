86f75c583fc84424f65e243f704e7aaae05cb728
DiogoSantosSilva <diogo_sdss@hotmail.com>
2024-11-06 20:00:13 -0300

    fix conflict

--- job/admin.py
+++ job/admin.py
@@ -4,3 +4,4 @@ from .models import Job
 admin.site.register(Job)
 
 # Register your models here.
+admin.site.register(Job)
--- job/views.py
+++ job/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Job
 
-# Create your views here.
 def test(request):
-    return HttpResponse("Hello world")
+     template_name = 'index.html'
+
+     jobs = Job.objects.all()
+
+     context = {"title": "Classificados", "jobs": jobs}
+     return render(request, template_name, context)
