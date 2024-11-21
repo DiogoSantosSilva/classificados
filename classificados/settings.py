0ecfee30b0b46e7c179caab3ebd8ee9a8c578a9f
FelipeDuarte <felipecduarte@hotmail.com>
2024-11-21 07:41:19 -0300

    criação do login-logout

--- classificados/settings.py
+++ classificados/settings.py
@@ -136,3 +136,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Adicionando as configurações de login/logout 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/' 
+LOGOUT_REDIRECT_URL = '/' 
--- job/views.py
+++ job/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Job
 from .form import JobForm
+from django.contrib.auth.decorators import login_required 
 
 def home(request):
     template_name = 'index.html'
     jobs = Job.objects.all()
     context = {"title": "Classificados", "jobs": jobs}
     return render(request, template_name, context)
-
 
 # GET
 def job(request, pk):
@@ -17,7 +17,21 @@ def job(request, pk):
     context = {"job": job}
     return render(request, template_name, context)
 
+@login_required
 def create_edit_job(request):
+    template_name = 'form.html'
+    if request.method == "POST":
+        form = JobForm(request.POST)
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.owner = request.user
+            job.save()
+            return HttpResponseRedirect("/")
+    else: 
+        form = JobForm()
+    return render(request, template_name, {"form": form})
+
+# def create_edit_job(request):
     template_name = 'form.html'
     if request.method == "POST":
         form = JobForm(request.POST)
